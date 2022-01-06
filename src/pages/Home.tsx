a9c33c65fb3c9c42398653dbde641ec435c0df01
jvolima <jvolima2004@gmail.com>
2022-01-06 14:06:23 -0300

    skills

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -58,7 +58,10 @@ export function Home() {
 					onChangeText={setNewSkill}
 				/>
 
-				<Button onPress={handleAddNewSkill}/>
+				<Button 
+					title="Add"
+					onPress={handleAddNewSkill}
+				/>
 
 				<Text style={[styles.title, { marginVertical: 50 }]}>
 					My Skills 
